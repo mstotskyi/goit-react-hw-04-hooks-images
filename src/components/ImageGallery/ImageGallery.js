9fc9539898ee534db725141374e68cea2eb445be
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-10-25 21:11:34 +0300

    видалив коментарі

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -51,7 +51,6 @@ export function ImageGallery({ searchQuery }) {
         setStatus('error');
       });
   };
-  // const togleSpiner = () => setShowSpiner(!showSpiner);
   const togleModal = () => setShowModal(!showModal);
   const openModal = e => {
     setShowModal(!showModal);
@@ -80,89 +79,6 @@ export function ImageGallery({ searchQuery }) {
     alert('ERROR!!');
   }
 }
-
-// export class ImageGallery extends Component {
-//   state = {
-//     searchResults: [],
-//     status: 'init',
-//     showModal: false,
-//     modalImage: '',
-//     showSpiner: false,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.searchQuery !== this.props.searchQuery) {
-//       this.setState({ status: 'pending' });
-//       newPicsApiService.query = this.props.searchQuery;
-//       newPicsApiService.resetPage();
-//       newPicsApiService
-//         .fetchPictures()
-//         .then(result =>
-//           this.setState({ searchResults: result, status: 'success' }),
-//         )
-//         .catch(error => {
-//           this.setState({ status: 'error' });
-//         });
-//     }
-//   }
-
-//   handleOnClick = e => {
-//     newPicsApiService.incrementPage();
-//     this.togleSpiner();
-//     newPicsApiService
-//       .fetchPictures()
-//       .then(result => {
-//         this.setState(prevState => ({
-//           searchResults: [...prevState.searchResults, ...result],
-//           status: 'success',
-//         }));
-//         window.scrollTo({
-//           top: document.documentElement.scrollHeight,
-//           behavior: 'smooth',
-//         });
-//         this.togleSpiner();
-//       })
-//       .catch(error => {
-//         this.setState({ status: 'error' });
-//       });
-//   };
-//   togleSpiner = () => this.setState({ showSpiner: !this.state.showSpiner });
-//   togleModal = () => this.setState({ showModal: !this.state.showModal });
-
-//   showModal = e => {
-//     this.togleModal();
-//     this.setState({ modalImage: e.target });
-//   };
-
-//   render() {
-//     if (this.state.status === 'init') {
-//       return <h1>Hallo! Search something!</h1>;
-//     }
-//     if (this.state.status === 'pending') {
-//       return <Spiner />;
-//     }
-//     if (this.state.status === 'success') {
-//       return (
-//         <>
-//           <ul className={styles.ImageGallery}>
-//             <ImageGalleryItem
-//               photos={this.state.searchResults}
-//               showModal={this.showModal}
-//             />
-//           </ul>
-//           {this.state.showSpiner && <Spiner />}
-//           <Button handleOnClick={this.handleOnClick} />
-//           {this.state.showModal && (
-//             <Modal togleModal={this.togleModal} img={this.state.modalImage} />
-//           )}
-//         </>
-//       );
-//     }
-//     if (this.state.status === 'error') {
-//       alert('ERROR!!');
-//     }
-//   }
-// }
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string,
